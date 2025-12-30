872140716200670caf4f37363b253a1c9d137cb9
BrikenaAhmeti <brikena.ba@gmail.com>
2025-12-31 00:45:29 +0100

    Changes to have nested layout for profile

---
+++ WP25G10/Areas/Identity/Pages/Account/Manage/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WP25G10.Areas.Identity.Pages.Account.Manage
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- WP25G10/obj/Release/net6.0/WP25G10.AssemblyInfo.cs
+++ WP25G10/obj/Release/net6.0/WP25G10.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WP25G10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2974ae354f49d32c65ef620d659a9e0df21e6121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afe87dcfe11b968c9c23b3b880c98a1359faa88")]
 [assembly: System.Reflection.AssemblyProductAttribute("WP25G10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WP25G10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
